55d1d4b8bc258ef40a129877797e7468d5b03d15
Shubham <shub_patel2295@yahoo.com>
2022-06-29 18:11:55 -0600

    Removed extra files

--- server/index.js
+++ server/index.js
@@ -5,28 +5,18 @@ const resolvers = require('./resolvers/resolvers');
 const getUser = require('./utils/getUser');
 
 const MONGODB = 'mongodb://localhost:27017/bank';
-let i = 1;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => {
-
-
-    console.log(i, req.headers);
-    i++;
     const token = req.headers.authorization || '';
-
     if (token) {
       const userId = getUser(token);
-      console.log('Setting up context', userId);
       return { userId };
     }
-
   }
 });
-
-
 
 mongoose.connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
